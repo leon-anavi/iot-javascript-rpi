3e92c6fd6ef61c86b426a6a70b05bad90b79526d
Leon Anavi <leon@anavi.org>
2018-10-05 11:18:24 +0300

    Add demo website
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ lab7-mqtt/website/js/demo.js
@@ -0,0 +1,91 @@
+var mqttClient;
+// Change the host according to your own setup
+var host = "192.168.4.66";
+var port = 1884;
+var path = "";
+var workgroup = "workgroup";
+
+var topicPrefix = "home/room/";
+var topicTemperature = topicPrefix + "temperature";
+var topicHumidity = topicPrefix + "humidity";
+var topicLight = topicPrefix + "light";
+
+function changeColor(red, yellow, green)
+{
+  var payload = '{ "r": '+red+', "y": '+yellow+', "g": '+green+' }';
+  message = new Paho.MQTT.Message(payload);
+  message.destinationName = topicLight;
+  mqttClient.send(message);
+}
+
+function onConnect() {
+  console.log("connected");
+  $('#txtInfo').removeClass('d-none');
+  $('#txtHost').text(host);
+  $('#txtPort').text(port);
+
+  mqttClient.subscribe(topicPrefix+"#");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("MQTT message arrive: "+message.destinationName);
+  console.log("payload: "+message.payloadString);
+  try {
+    var data = JSON.parse(message.payloadString);
+
+    if (topicTemperature == message.destinationName) {
+      $('#txtTemperature').text("Temperature: "+data.temperature+"C");
+    }
+
+    if (topicHumidity == message.destinationName) {
+      $('#txtHumidity').text("Humidity: "+data.humidity+"%");
+    }
+  } catch (e) {
+    console.log("Malformed data");
+  }
+}
+
+$(document).ready(function() {
+    var mqttClientId = "MqttDemo" + Math.floor((Math.random() * 1000) + 1);
+    mqttClient = new Paho.MQTT.Client(host, Number(port), path, mqttClientId);
+
+    // set callback handlers
+    mqttClient.onConnectionLost = onConnectionLost;
+    mqttClient.onMessageArrived = onMessageArrived;
+
+    // connect the client
+    mqttClient.connect({onSuccess:onConnect});
+
+    $('#buttonColorAll').on('click', function (e) {
+         e.preventDefault();
+         changeColor(true, true, true);
+    });
+
+    $('#buttonColorRed').on('click', function (e) {
+         e.preventDefault();
+         changeColor(true, false, false);
+    });
+
+    $('#buttonColorGreen').on('click', function (e) {
+         e.preventDefault();
+         changeColor(false, false, true);
+    });
+
+    $('#buttonColorYellow').on('click', function (e) {
+         e.preventDefault();
+         changeColor(false, true, false);
+    });
+
+    $('#buttonColorOff').on('click', function (e) {
+         e.preventDefault();
+         changeColor(false, false, false);
+    });
+});
